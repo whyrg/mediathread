d9d2b03fabfb5cae51f889f9e225c969b64e6adf
Richard Guo <yrguo@stanford.edu>
2022-02-02 17:05:32 -0800

    Dedup variable names

--- mediathread/settings_docker.py
+++ mediathread/settings_docker.py
@@ -19,11 +19,9 @@ DB_USER = os.environ.get('DB_USER', '')
 DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
 
 AWS_S3_CUSTOM_DOMAIN = os.environ.get('AWS_S3_CUSTOM_DOMAIN', '')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
-AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY', None)
-AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY', None)
-AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY
-AWS_SECRET_ACCESS_KEY = AWS_SECRET_KEY
+AS3_PRIVATE_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY', None)
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_KEY', None)
 
 RAVEN_DSN = os.environ.get('RAVEN_DSN', '')
 
